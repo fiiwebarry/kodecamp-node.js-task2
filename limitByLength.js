c7931cfd4399998bddc04db4d945dde747a0ff19
fiiwebarry <fiiwebarry@yahoo.com>
2024-06-09 21:31:09 +0100

    update

--- limitByLength.js
+++ limitByLength.js
@@ -1,16 +1,12 @@
+let sentence =
+  'ut sem nulla pharetra diam sit amet nisl suscipit adipiscing bibendum est ultricies integer quis auctor elit sed vulputate mi sit amet mauris commodo quis imperdiet massa tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin libero nunc consequat.';
 
-let sentence='ut sem nulla pharetra diam sit amet nisl suscipit adipiscing bibendum est ultricies integer quis auctor elit sed vulputate mi sit amet mauris commodo quis imperdiet massa tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin libero nunc consequat.'
-
-function limitByLength(items)
-{
-
-let words = items.split(' ');
- if(words.length < 100){
-    return words.join(' ');
-}
- else{
-    return words.slice(0, 100).join(' ');
+function limitByLength(items) {
+  let words = items;
+  if (words.length <= 100) {
+    return words;
+  }
+  return words.slice(0, 100) + '...';
 }
 
-}
-console.log(limitByLength(sentence))
+console.log(limitByLength(sentence));
